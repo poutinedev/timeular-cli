7bc03c728a6d597f358198885fb4a2c183687cb6
Chris Lagasse <soben@outlook.com>
2020-09-18 21:23:19 -0400

    WIP.

--- test/timeular.js
+++ test/timeular.js
@@ -1,5 +1,11 @@
 const assert = require('chai').assert
+// const fetchMock = require('fetch-mock')
 
 describe("Timeular API", () => {
-  
+  describe("requiring keys", () => {
+    it ("should not pass the key validation check", () => {
+      process.env.TIMEULAR_API_KEY = null;
+      assert.throws(require('../src/timeular/index'), Error, "Missing API Key and Secret. Please provide with environment variables TIMEULAR_API_KEY and TIMEULAR_API_SECRET");
+    })
+  })
 })
